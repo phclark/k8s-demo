2255b67f8481b60373f94b8a0833e8046421ab8e
Philip Clark <philip.alan.clark@gmail.com>
2022-01-25 21:07:24 -0500

    services and charts folders

---
+++ services/README.md
@@ -0,0 +1,1 @@
+The docker-compose.yaml is used for two purposes, to define build sources and to create a local development environment. 
---
+++ services/demo-api/test/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Automate all the things!",
+        "timestamp": 1529729125
+    }
