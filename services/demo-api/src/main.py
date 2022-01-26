06e7ec0fb5c99ad68fd35e3efe138a6812fb1991
Philip Clark <philip.alan.clark@gmail.com>
2022-01-26 14:56:39 -0500

    add prometheus instrumentation to demo-api

--- services/demo-api/src/main.py
+++ services/demo-api/src/main.py
@@ -1,8 +1,12 @@
 from typing import Optional
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
+
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def demo_endpoint():
