2eae377c832f11621371092f2f071f788b7f05e6
Philip Clark <philip.alan.clark@gmail.com>
2022-01-30 09:26:44 -0500

    add version header

--- services/demo-api/src/main.py
+++ services/demo-api/src/main.py
@@ -1,15 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+import os
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 
-
 Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
-def demo_endpoint():
+def demo_endpoint(response: Response):
+    response.headers["X-VERSION"] = os.environ.get('VERSION', 'default')
     return {
         "message": "Automate all the things!",
         "timestamp": 1529729125
