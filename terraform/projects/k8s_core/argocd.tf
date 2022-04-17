251abf77c73c133a2a4dbddd306bec2544c7e8a6
Philip Clark <philip.clark@iterable.com>
2022-04-17 13:30:29 -0400

    fix namespace

--- terraform/projects/k8s_core/argocd.tf
+++ terraform/projects/k8s_core/argocd.tf
@@ -3,6 +3,7 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "3.31.0"
+  namespace  = "argocd"
 
   create_namespace = true
 
