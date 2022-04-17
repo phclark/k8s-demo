54b4f6e8a9b91bb6a5e0b47d28646eda5359fba7
Philip Clark <philip.clark@iterable.com>
2022-04-17 10:18:15 -0400

    create monitoring namespace

--- terraform/projects/k8s_core/secrets.tf
+++ terraform/projects/k8s_core/secrets.tf
@@ -1,11 +1,17 @@
-resource "kubernetes_secret" "example" {
+resource "kubernetes_namespace" "monitoring" {
   metadata {
-    name = "grafana-admin-credentials"
-    namespace = "monitoring"
+    name = "monitoring"
+  }
+}
+
+resource "kubernetes_secret" "grafana_admin_credentials" {
+  metadata {
+    name      = "grafana-admin-credentials"
+    namespace = kubernetes_namespace.monitoring.name
   }
 
   data = {
-    admin-user = "admin"
+    admin-user     = "admin"
     admin-password = data.aws_ssm_parameter.grafana_admin_password.value
 
   }
