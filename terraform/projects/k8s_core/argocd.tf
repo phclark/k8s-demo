cefb2ef7849e349f682976c9db1e1bb2604497a2
Philip Clark <philip.clark@iterable.com>
2022-04-17 13:28:20 -0400

    use values file

---
+++ terraform/projects/k8s_core/argocd.tf
@@ -0,0 +1,34 @@
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "3.31.0"
+
+  create_namespace = true
+
+  values = [
+    templatefile("${path.module}/argocd-values.yaml", {
+    })
+  ]
+}
+
+data "aws_secretsmanager_secret" "github_private_key" {
+  name = "github-private-key"
+}
+
+resource "kubernetes_secret" "github_private_key" {
+  metadata {
+    name = "github-private-key"
+  }
+
+  namespace = "argocd"
+  labels = {
+    "argocd.argoproj.io/secret-type" = "repository"
+  }
+
+  data = {
+    type          = "git"
+    url           = "git@github.com:hclark/k8s-demo"
+    sshPrivateKey = data.aws_secretsmanager_secret.github_private_key.value
+  }
+}
