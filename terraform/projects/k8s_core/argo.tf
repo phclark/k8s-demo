7a829d6a5eb940c9ef22066b6332c15afd55a8f4
Philip Clark <philip.clark@iterable.com>
2022-04-17 13:04:50 -0400

    use hosted chart

--- terraform/projects/k8s_core/argo.tf
+++ terraform/projects/k8s_core/argo.tf
@@ -1,6 +1,23 @@
 resource "helm_release" "argocd" {
-  name  = "argocd"
-  chart = "../../../../../charts/argo-cd"
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "3.31.0"
 
   create_namespace = true
+
+  set {
+    name  = "argo-cd.global.image.tag"
+    value = "v2.2.3"
+  }
+
+  set {
+    name  = "argo-cd.dex.enabled"
+    value = false
+  }
+
+  set {
+    name  = "argo-cd.server.extraArgs"
+    value = ["--insecure"]
+  }
 }
