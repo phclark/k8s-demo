261fa2db64bdd0f5a5f886cf98ca01c5217db982
Philip Clark <philip.clark@iterable.com>
2022-04-17 23:14:16 -0400

    enable_host_networking

--- terraform/projects/k8s_core/ingress.tf
+++ terraform/projects/k8s_core/ingress.tf
@@ -7,8 +7,9 @@ module "alb_ingress_controller" {
   k8s_cluster_type = "eks"
   k8s_namespace    = "kube-system"
 
-  aws_region_name  = data.aws_region.current.name
-  k8s_cluster_name = data.aws_eks_cluster.target.name
+  aws_region_name        = data.aws_region.current.name
+  k8s_cluster_name       = data.aws_eks_cluster.target.name
+  enable_host_networking = true
 
   alb_controller_depends_on = []
 }
