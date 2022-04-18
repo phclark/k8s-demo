0c3f6477949947953852b87ebee8b7a1ec475b92
Philip Clark <philip.clark@iterable.com>
2022-04-18 09:37:27 -0400

    use kube-system

--- terraform/projects/k8s_core/ingress.tf
+++ terraform/projects/k8s_core/ingress.tf
@@ -3,7 +3,7 @@ module "alb_ingress_controller" {
   aws_load_balancer_controller_chart_version = "1.4.1"
 
   k8s_cluster_type = "eks"
-  # k8s_namespace    = "kube-system"
+  k8s_namespace    = "kube-system"
 
   aws_region_name  = data.aws_region.current.name
   k8s_cluster_name = data.aws_eks_cluster.target.name
