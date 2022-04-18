54cbc349da714d5f9d401e44cbc78f1455255734
Philip Clark <philip.clark@iterable.com>
2022-04-18 09:34:54 -0400

    use local module

--- terraform/projects/k8s_core/ingress.tf
+++ terraform/projects/k8s_core/ingress.tf
@@ -1,16 +1,13 @@
 module "alb_ingress_controller" {
-  count = 0
-  # source  = "iplabs/alb-ingress-controller/kubernetes"
-  # version = "3.4.0"
-  source                                     = "github.com/GSA/terraform-kubernetes-aws-load-balancer-controller.git?ref=v5.0.1"
+  source                                     = "./alb-ingress-controller"
   aws_load_balancer_controller_chart_version = "1.4.1"
 
   k8s_cluster_type = "eks"
-  k8s_namespace    = "kube-system"
+  # k8s_namespace    = "kube-system"
 
-  aws_region_name        = data.aws_region.current.name
-  k8s_cluster_name       = data.aws_eks_cluster.target.name
-  enable_host_networking = true
+  aws_region_name  = data.aws_region.current.name
+  k8s_cluster_name = data.aws_eks_cluster.target.name
+  # enable_host_networking = true
 
   alb_controller_depends_on = []
 }
