4fe1dd3586dbf8ea5bcdd1b3d9f624791ce64dc7
Philip Clark <philip.clark@iterable.com>
2022-04-27 09:49:35 -0400

    fixes

--- terraform/projects/cluster/s3.tf
+++ terraform/projects/cluster/s3.tf
@@ -21,7 +21,9 @@ resource "aws_s3_bucket" "helm" {
 }
 
 resource "aws_s3_bucket_public_access_block" "helm" {
-  bucket              = aws_s3_bucket.helm.id
-  block_public_acls   = true
-  block_public_policy = true
+  bucket                  = aws_s3_bucket.helm.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
