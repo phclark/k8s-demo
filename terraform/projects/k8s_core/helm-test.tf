c29b1e504df0c50f16bca10c93e938c7aa3750ea
Philip Clark <philip.clark@iterable.com>
2022-04-28 16:50:38 -0400

    remove helm test

--- terraform/projects/k8s_core/helm-test.tf
+++ terraform/projects/k8s_core/helm-test.tf
@@ -1,8 +1,8 @@
-resource "helm_release" "test" {
-  name       = "demo-api"
-  repository = "s3://k8s-demo-dev-helm-charts"
-  chart      = "demo-api"
-  version    = "0.1.0"
+# resource "helm_release" "test" {
+#   name       = "demo-api"
+#   repository = "s3://k8s-demo-dev-helm-charts"
+#   chart      = "demo-api"
+#   version    = "0.1.0"
 
-  depends_on = [null_resource.helm_plugin]
-}
+#   depends_on = [null_resource.helm_plugin]
+# }
